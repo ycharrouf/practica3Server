cbfed2b52789f758f8bc2a0fe8a1088f35d7214a
ycha0707 <ycha0707@g.educaand.es>
2024-10-28 21:57:09 +0100

    todos los archivos

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web de Yassin Charrouf Errynda</title>
+</head>
+<body>
+    <!-- php no funciona -->
+    <h1>Yassin Charrouf Errynda</h1>
+    <?php
+        date_default_timezone_set('Europe/Madrid');
+        echo date('H:i:s');
+    ?>
+    <div id="relojCliente"></div>
+    <script src="./js/reloj.js"></script>
+
+    <form action="insertar.php" method="POST">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre" required>
+        <label for="edad">Edad:</label>
+        <input type="number" name="edad" id="edad" min="1" required>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ insertar.php
@@ -0,0 +1,27 @@
+<?php
+    $servername = "127.0.0.1";
+    $username = "prueba";
+    $password  = "1234";
+    $dbname = "prueba";
+
+    /* Crear una conexión con la base de datos */
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    /* Comprobar la conexión */
+    if ($conn->connect_error) {
+        die("Error en la conexión: " . $conn->connect_error);
+    }
+
+    /* Comprobar si se ha enviado el formulario */
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombre = $_POST["nombre"];
+        $edad = $_POST["edad"];
+        $sql = "INSERT INTO persona (nombre, edad) VALUES ('$nombre', $edad)";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Datos insertados correctamente";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+?>
